e348b8147488243a323a60118899fd65231375c3
Otávio Francisco <105547941+otavio-hbf@users.noreply.github.com>
2022-09-13 09:53:37 -0300

    Update questao1.cpp

--- questao1.cpp
+++ questao1.cpp
@@ -6,33 +6,39 @@
 #include <pthread.h>
 #include <time.h>
 
+pthread_mutex_t mutex;
+
 
 class Candidato{
 
     private:
         int num_de_votos;
         float percentual;
+        pthread_mutex_t mutex_inc;
 
     public:
         Candidato(){
             num_de_votos = 0;
             percentual = 0;
+            pthread_mutex_init(&mutex_inc, NULL);
         }
+
+        //vou criar um destrutor kkkk pthread_mutex_destroy(&mutex_inc);
 
         void getPercentual(int totalVotos){
             percentual = ((float)num_de_votos / (float)totalVotos) * 100;
         }
 
         void incVotos(){
-            pthread_mutex_t mutex;
-            pthread_mutex_lock(&mutex);
+            
+            pthread_mutex_lock(&mutex_inc);
             num_de_votos++;
-            pthread_mutex_unlock(&mutex);
-            pthread_mutex_destroy(&mutex);
+            pthread_mutex_unlock(&mutex_inc);
+            
         }
 
         void printCandidato(int n){
-            printf("candidato %d: %.2f porcento (%d votos)\n", n, percentual, num_de_votos);
+            printf("candidato %d: %.2f%%(%d votos)\n", n, percentual, num_de_votos);
         }
 };
 
@@ -62,15 +68,15 @@ Arquivo getFileName(int numArquivo){
     char numString[10];
     sprintf(numString, "%d", numArquivo);
     char extension[5];
-    sprintf(extension, ".txt");
+    sprintf(extension, ".in");
     arq.tamanhoNome = strlen(numString) + strlen(extension) + 1;
     snprintf(arq.nome, arq.tamanhoNome, "%s%s", numString, extension);
 
     return arq;
 }
 
-//-------------------tá pegando------------------------------------------
-void CreateFile(int numArquivo, int qtdCandidatos){
+//----------------- precisa alterar -----------------------------------------
+void CreateFile(int numArquivo, int qtdCandidatos){ // convém fazer com que essa função retorne um ponteiro de arquivo
 
     Arquivo arq = getFileName(numArquivo);
    
@@ -88,37 +94,33 @@ void CreateFile(int numArquivo, int qtdCandidatos){
 
 
 void* ContaVoto(void* arguments){
-    pthread_mutex_t mutex;
+    pthread_mutex_lock(&mutex);
+       
     ArgStruct* arg = (ArgStruct*) arguments;
-
-    printf("O arquivo %s está sendo contado\n", arg->nomeArquivo);
-
-    arg->fptr = fopen((arg->nomeArquivo),"r");
     int voto;
+
+    
+    
+    arg->fptr = fopen((arg->nomeArquivo),"r");
+    
     //Essa provavelmente vai ser a função chamada pela thread
     while (!feof(arg->fptr)){
-
-        pthread_mutex_lock(&mutex);
+       
         *(arg-> qtd_votos)++; //vai precisar de mutex tbm né
         fscanf(arg->fptr, "%d", &voto);
+        printf("%d,", voto); //a contagem tá pegando!
         arg-> arrayDeCandidatos[voto].incVotos(); //seria aqui que tá a região crítica ???? com certeza....
-        pthread_mutex_unlock(&mutex);
-        pthread_mutex_destroy(&mutex);
-
-
 
     }
+    printf("O arquivo %s está sendo contado\n", arg->nomeArquivo);
+
+    pthread_mutex_unlock(&mutex);
+    
 }
-/*0
-1
-0
-2
-2
-3
-*/
 
 int main(){
-
+    pthread_mutex_init(&mutex, NULL);
+    
     //----------------tá pegando----------------------------------------------//
     int qtd_votos = 0, N, T , G;
     int* totalDeVotos = &qtd_votos;
@@ -129,13 +131,14 @@ int main(){
     scanf("%d", &T);    
     printf("Digite um número C de candidatos ao Governo: ");
     scanf("%d", &G);
+    printf("\n\n\n");
     
     FILE* ponteiroArquivo[N];
     pthread_t thread[T]; // criando as threads
-    Candidato candidatos[G]; // c + 1
+    Candidato candidatos[G + 1]; // c + 1
 
     for(int i=1; i <= N; i++){
-        CreateFile(i, G);
+        CreateFile(i, G); // create file precisa usaro array de ponteiro pra arquivos!!!!!
     }
     //---------------------------------------------------------------------------//
 
@@ -153,24 +156,37 @@ int main(){
     // 4 arquivos
     // 3 threads
     //5 candidatos
-
-    while(N){
-        for(int i=0; i < T && i < N; i++){ //
-            Arquivo arq = getFileName(N-i); // vai dar merda, porque vai dar um número negativo
-            printf("thread %d para o arquvivo %s\n" , i, arq.nome); //debug
+    int conclude_flag = 0;
+    while(N){ //n= 4 t = 3 // n=1 t=3
+        
+        for(int i=0; i < T && i < N; i++){
+            Arquivo arq = getFileName(N-i);
+            printf("thread %d para o arquivo %s\n" , i, arq.nome); //debug
             ArgStruct args = {ponteiroArquivo[N - i - 1], totalDeVotos, candidatos, (char*) arq.nome};
-            pthread_create(&thread[i], NULL, &ContaVoto, (void*) &args);
+            pthread_create(&thread[i], NULL, ContaVoto, (void*) &args);
+            // printf("thread %d\n", i);
 
         }
         
-        if(N >= T) N = N - T;
-        else N = T;
-
-        for(int i=0; i < T ; i++){
-            pthread_join(thread[i], NULL);
-            printf("Thread %d terminou \n", i);
+        for(int j=0; j < T && j < N; j++){
+            pthread_join(thread[j], NULL);
         }
+
+        if(N >= T){
+            N = N - T; //4 - 3 = 1
+        }
+        else{
+            N = 0;
+        } 
+        // printf("n: %d t: %d\n", N, T );
+
+        
+        //quando coloco esse trecho de código o único arquivo que aparenta ser lido é o 2 (usando o ex de 4 3 5)
+
     }
+
+    pthread_mutex_destroy(&mutex);
+    
 
     // for (int i = 0; i < G; i++) candidatos[i].getPercentual(qtd_votos);
     // for (int i = 0; i < G; i++) candidatos[i].printCandidato(i);
