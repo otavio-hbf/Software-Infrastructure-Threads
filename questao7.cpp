6ed245732e63372a5f5b87ff06c144b69d3a30d7
Otávio Francisco <105547941+otavio-hbf@users.noreply.github.com>
2022-09-13 23:28:56 -0300

    Update questao7.cpp

--- questao7.cpp
+++ questao7.cpp
@@ -4,31 +4,79 @@
 #include <pthread.h>
 #include <time.h>
 
+typedef struct Arg{
+    int** matrix;
+    int size;
+    int index;
+}Arg;
 
-int main(){
-    int n;
-    printf("Qual o tamanho n da matriz ?");
-    std::cin >> n;
+int** getMatrix(int n){
 
-    char matriz[n][n];
-    pthread_t th_horizontal[n];
-    pthread_t th_vertical[n];
+    //----- Alocação Dinâmica da matriz quadrada --------------------
 
-    
-//----- Código que gera e printa uma matriz aleatória------------
+    int** matrix = (int**) malloc( n * sizeof(int*));
+    for(int i=0; i < n; i++) matrix[i] = (int*) malloc(n * sizeof(int));
+
+    //----- Código que gera e printa uma matriz aleatória------------
 
     srand(time(0));
     for(int i=0; i < n ; i++){
         for(int j=0; j < n; j++){
-            matriz[i][j] = (rand() % 93) + 33; //possibilita que ela seja criada apenas com caracteres visíveis
-            printf("[%c]\t", matriz[i][j]);
+            matrix[i][j] = (rand() % 93) + 33; //possibilita que ela seja criada apenas com caracteres visíveis
+            printf("[%c]\t", matrix[i][j]);
         }
         printf("\n");
     }
 
-    for(int i=0; i < n; i++){
+    return matrix;
+
+}
+
+void* check_row(int** matrix, int size, int row){
+
+    int* detect_flag = (int*) malloc(sizeof(int));
+
+    bool* check_array_row;
+    check_array_row = (bool*) calloc(93, sizeof(bool));
+
+    for(int j=0; j < size; j++){
+        int ascii_code = matrix[row][j];
         
+        if(!check_array_row[ascii_code]) {check_array_row[ascii_code] = 1;}
+        else{
+            *detect_flag = 1;
+            j = size;
+        };
+
     }
 
+    return (void*) detect_flag;
+    
+}
+
+void check_column(){
+    bool check_array_column [93]; 
+}
+
+int main(){
+
+    int n;
+    printf("Qual o tamanho n da matriz ?");
+    std::cin >> n;
+
+    int** square = getMatrix(n);
+
+    pthread_t th_rows[n];
+    pthread_t th_columns[n];
+
+    for(int i=0; i < n; i++){
+        Arg argument = {square, n, i};
+        pthread_create(&th_rows[i], NULL, &check_row, (void*) &argument);
+    } 
+
+
+    
+
+    
     
 }
